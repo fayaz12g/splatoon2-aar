489278d2462bacdab4569203028f5c9ed350db5d
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-03-20 20:17:46 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -315,7 +315,7 @@ def select_mario_folder():
 
     # Create the PCHTXT Files
     visual_fixes = create_visuals(do_screenshot.get(), do_disable_fxaa.get(), do_disable_dynamicres.get())
-    create_patch_files(patch_folder, str(ratio_value), str(scaling_factor), visual_fixes)
+    # create_patch_files(patch_folder, str(ratio_value), str(scaling_factor), visual_fixes)
     romfs_folder = os.path.join(input_folder, mod_name, "romfs", "Layout")
     theromfs_folder = os.path.join(input_folder, mod_name, "romfs")
 
